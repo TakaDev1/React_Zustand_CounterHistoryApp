bc6f6152f8f14e387e8ebc6a914c1af37da55ce1
Izumi Takayuki <r8quattrospider@gmail.com>
2026-08-20 13:30:18 +0900

    feat: integrate Counter History into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 
 import './App.css'
+import Counter from './components/Counter'
+import History from './components/History'
 
 function App() {
 
 
   return (
     <>
-      
+      <h1>CounterHistoryAPp</h1>
+      <Counter />
+      <History />
     </>
   )
 }
