df35c5ec50583abbea4c65824e2782b1e5baa87e
Izumi Takayuki <r8quattrospider@gmail.com>
2026-08-20 09:09:49 +0900

    feat: create history component

---
+++ src/components/History.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useCountStore } from '../stores/CounterStore';
+
+const History = () => {
+    const {history, resetHistory} = useCountStore();
+  return (
+    <div>
+        {/* 履歴一覧 */}
+        {history.length > 0 ? (
+            <ul>
+                {history.map(elem => (
+                    <li className='list-none'>{elem}</li>
+                ))}
+            </ul>
+        ) : (
+            <p>履歴がありません</p>
+        )}
+        {/* リセットボタン */}
+        <button onClick={resetHistory}>リセット</button>
+    </div>
+  );
+};
+
+export default History;
