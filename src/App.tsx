dfb91fca502536f09855b81968d03a8b016d44a6
Izumi Takayuki <r8quattrospider@gmail.com>
2026-08-20 15:22:54 +0900

    style: adjust counter and histroy UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-
-import './App.css'
-import Counter from './components/Counter'
-import History from './components/History'
+import "./App.css";
+import Counter from "./components/Counter";
+import History from "./components/History";
 
 function App() {
-
-
   return (
     <>
-      <h1>CounterHistoryAPp</h1>
-      <Counter />
-      <History />
+      <div className="min-h-screen flex flex-col justify-center items-center border">
+        <h1 className="mb-10">CounterHistoryApp</h1>
+        <Counter />
+        <History />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,15 +1,34 @@
-import React, { useState } from 'react';
-import { useCountStore } from '../stores/CounterStore';
+import React, { useState } from "react";
+import { useCountStore } from "../stores/CounterStore";
 
 const Counter = () => {
-  const {count, stepCount, increment, decrement, setStepCount} = useCountStore();
+  const { count, stepCount, increment, decrement, setStepCount } = useCountStore();
 
   return (
-    <div>
-      <p>Count: {count}</p>
-      <input type="text" value={stepCount} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setStepCount(Number(event.target.value))} />
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
+    <div className=" px-5 py-5 w-full">
+      <p className={`!mb-10 text-xl font-bold ${count >= 0 ? "text-green-500" : "text-red-500"}`}>
+        Count: {count}
+      </p>
+      <input
+        type="text"
+        value={stepCount}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          setStepCount(Number(event.target.value))
+        }
+        className="border py-2 text-center w-auto text-xl"
+      />
+      <button
+        onClick={increment}
+        className="px-4 py-2 ml-10 mr-2 rounded-lg bg-blue-500 text-white cursor-pointer hover:opacity-70"
+      >
+        +
+      </button>
+      <button
+        onClick={decrement}
+        className="px-4 py-2 rounded-lg bg-red-500 text-white cursor-pointer hover:opacity-70"
+      >
+        -
+      </button>
     </div>
   );
 };
